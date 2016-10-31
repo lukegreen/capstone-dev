448cd25ad4ce4866743eca26cd23d2590312ca5a
Luke Green <elukuent@gmail.com>
2016-10-30 20:13:28 -0700

    configured grunt buildcontrol

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,8 @@ module.exports = function (grunt) {
   require('jit-grunt')(grunt, {
     useminPrepare: 'grunt-usemin',
     ngtemplates: 'grunt-angular-templates',
-    cdnify: 'grunt-google-cdn'
+    cdnify: 'grunt-google-cdn',
+    buildcontrol: 'grunt-build-control'
   });
 
   // Configurable paths for the application
@@ -30,6 +31,22 @@ module.exports = function (grunt) {
 
     // Project settings
     yeoman: appConfig,
+
+    //Buildcontrol settings
+    buildcontrol: {
+     options: {
+       dir: 'dist',
+       commit: true,
+       push: true,
+       message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+     },
+     pages: {
+       options: {
+         remote: 'git@github.com:lukegreen/capstone-dev.git',
+         branch: 'gh-pages'
+       }
+     }
+   },
 
     // Watches files for changes and runs tasks based on the changed files
     watch: {
