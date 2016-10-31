1ed32d28170481d2e962c221c69e28825fee6cd9
Luke Green <elukuent@gmail.com>
2016-10-30 20:04:54 -0700

    angular bootstrapping

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name capstoneDevApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the capstoneDevApp
+ */
+angular.module('capstoneDevApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
