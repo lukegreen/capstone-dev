72c12b7ca72bcfb27521e86eb3c080dfe969d04a
Luke Green <elukuent@gmail.com>
2016-11-05 14:22:42 -0700

    API wired
    Got the API to send data...asynchronous call, so needed then()

--- app/scripts/controllers/manifest.js
+++ app/scripts/controllers/manifest.js
@@ -10,13 +10,18 @@
 angular.module('capstoneDevApp')
   .controller('ManifestCtrl', function ($scope, roverData) {
 
-    console.log('hello');
-
     //get data from API
-    $scope.roverVariable = roverData.query();
-    var roverVariable = $scope.roverVariable;
-    var problemTest = roverVariable.photos;
-    console.log(problemTest);
+    $scope.roverData = roverData.query();
+    $scope.roverData.$promise.then(function(data){
+    console.log(data);
+    console.log(data.photos[0]);
 
-
+    $scope.roverName = data.photos[0].rover.name;
+    $scope.roverLaunchDate = data.photos[0].rover.launch_date;
+    $scope.roverLandingDate = data.photos[0].rover.landing_date;
+    $scope.roverStatus = data.photos[0].rover.status;
+    $scope.roverTotalPhotos = data.photos[0].rover.total_photos;
+    console.log($scope.roverStatus);
   });
+
+});
