86626d5e49950bdc04d86ee301f752f9c5b1d43c
Luke Green <elukuent@gmail.com>
2016-11-16 14:43:33 -0800

    photos working

--- app/scripts/controllers/manifest.js
+++ app/scripts/controllers/manifest.js
@@ -11,20 +11,28 @@ angular.module('capstoneDevApp')
   .controller('ManifestCtrl', function ($scope, roverData) {
 
     //get data from API
-    $scope.roverData = roverData.query();
-    $scope.roverData.$promise.then(function(data){
-    console.log(data);
-    console.log(data.photos[0]);
+    function init(){
+      $scope.roverData = roverData.query();
+      $scope.roverData.$promise.then(function(data){
+        $scope.maxSol = data.photos[0].rover.max_sol;
+        $scope.roverDataTwo = roverData.query({
+          maxSol: $scope.maxSol
+        });
+        $scope.roverDataTwo.$promise.then(function(data){
+        //Manifest
+        $scope.roverName = data.photos[0].rover.name;
+        $scope.roverLaunchDate = data.photos[0].rover.launch_date;
+        $scope.roverLandingDate = data.photos[0].rover.landing_date;
+        $scope.roverStatus = data.photos[0].rover.status;
+        $scope.roverTotalPhotos = data.photos[0].rover.total_photos;
+        console.log(data.photos[0].sol);
+        //Most recent photo
+        $scope.mostRecent = data.photos[0].img_src;
+        console.log($scope.mostRecent);
+      });
+    });
+  }
 
-    //Manifest
-    $scope.roverName = data.photos[0].rover.name;
-    $scope.roverLaunchDate = data.photos[0].rover.launch_date;
-    $scope.roverLandingDate = data.photos[0].rover.landing_date;
-    $scope.roverStatus = data.photos[0].rover.status;
-    $scope.roverTotalPhotos = data.photos[0].rover.total_photos;
-    console.log($scope.roverStatus);
-  });
-
-
+  init();
 
 });
