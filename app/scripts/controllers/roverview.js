6a0a7ba707e02e29fe1c84ac513b04cd1eec5653
Luke Green <elukuent@gmail.com>
2016-11-29 16:38:01 -0800

    trying to create roverView

--- app/scripts/controllers/roverview.js
+++ app/scripts/controllers/roverview.js
@@ -9,38 +9,37 @@
  */
 angular.module('capstoneDevApp')
   .controller('RoverviewCtrl', function ($scope, roverData) {
-    //get data from API
-    function init(){
-      $scope.roverData = roverData.query({roverName: $scope.roverName});
-      $scope.roverData.$promise.then(function(data){
-        $scope.maxSol = data.photos[0].rover.max_sol;
-        $scope.roverDataTwo = roverData.query({
-          maxSol: $scope.maxSol
+
+
+    $scope.setName = function(clickEvent) {
+      $scope.roverName = clickEvent.currentTarget.name;
+      console.log($scope.roverName);
+
+      //get data from API
+      function init(){
+        $scope.roverData = roverData.query({roverName: $scope.roverName});
+        $scope.roverData.$promise.then(function(data){
+          $scope.maxSol = data.photos[0].rover.max_sol;
+          $scope.roverDataTwo = roverData.query({
+            maxSol: $scope.maxSol
+          });
+          $scope.roverDataTwo.$promise.then(function(data){
+
+          //Manifest
+          //$scope.roverName = data.photos[0].rover.name;
+          $scope.roverLaunchDate = data.photos[0].rover.launch_date;
+          $scope.roverLandingDate = data.photos[0].rover.landing_date;
+          $scope.roverStatus = data.photos[0].rover.status;
+          $scope.roverTotalPhotos = data.photos[0].rover.total_photos;
+          console.log(data.photos[0].sol);
+          //Most recent photo
+          $scope.mostRecent = data.photos[0].img_src;
+          console.log($scope.mostRecent);
         });
-        $scope.roverDataTwo.$promise.then(function(data){
-
-        //Manifest
-        $scope.roverName = data.photos[0].rover.name;
-        $scope.roverLaunchDate = data.photos[0].rover.launch_date;
-        $scope.roverLandingDate = data.photos[0].rover.landing_date;
-        $scope.roverStatus = data.photos[0].rover.status;
-        $scope.roverTotalPhotos = data.photos[0].rover.total_photos;
-        console.log(data.photos[0].sol);
-        //Most recent photo
-        $scope.mostRecent = data.photos[0].img_src;
-        console.log($scope.mostRecent);
-        //Insert relevant history
-        if ($scope.roverName === "Curiosity") {
-          document.getElementById("historyPara").innerHTML("<p>Info about Curiosity</p>");
-        } else if ($scope.roverName === "Opportunity") {
-          document.getElementById("historyPara").innerHTML("<p>Info about Opportunity</p>");
-        } else {
-          document.getElementById("historyPara").innerHTML("<p>Info about Spirit</p>");
-        }
       });
-    });
-  }
+    }
 
-  init();
+    init();
 
+  };
 });
